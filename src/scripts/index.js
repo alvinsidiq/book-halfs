5cc0510b3f2aab8282dd8142fe721055be89f4c1
alvin sidiq <123379611+alvinsidiq@users.noreply.github.com>
2024-05-01 20:46:22 +0700

    Add files via upload

---
+++ src/scripts/index.js
@@ -0,0 +1,4 @@
+/* eslint-disable linebreak-style */
+import 'regenerator-runtime';
+// eslint-disable-next-line linebreak-style
+console.log('Hello Coders!');
---
+++ webpack.prod.js
@@ -0,0 +1,25 @@
+
+const { merge } = require('webpack-merge');
+// eslint-disable-next-line linebreak-style
+const common = require('./webpack.common');
+
+module.exports = merge(common, {
+  mode: 'production',
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+            },
+          },
+        ],
+      },
+    ],
+  },
+});
